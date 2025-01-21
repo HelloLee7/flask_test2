e6a96e3b51c8d76215a253b27b4ed6da6e3c6eb2
HelloLee7 <jhelee920@gmail.com>
2025-01-21 16:06:51 +0900

    추천1번 업데이트

--- projects/static/js/gaechu.js
+++ projects/static/js/gaechu.js
@@ -6,5 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
     recommendBtn.addEventListener('click', function() {
         count++;
         recommendCount.textContent = count;
+
+        // animate__animated 클래스와 animate__rubberBand 클래스 추가
+        recommendBtn.classList.add('animate__animated', 'animate__rubberBand');
+
+        // 애니메이션이 끝나면 클래스 제거
+        recommendBtn.addEventListener('animationend', () => {
+            recommendBtn.classList.remove('animate__animated', 'animate__rubberBand');
+        }, { once: true });
     });
 });
