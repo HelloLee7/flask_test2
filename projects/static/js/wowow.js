bf465359a4f07b1a047e9e2f19d31189fe142ff2
HelloLee7 <jhelee920@gmail.com>
2025-01-22 14:23:40 +0900

    animation 수정

--- projects/static/js/wowow.js
+++ projects/static/js/wowow.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const animatedLinks = document.querySelectorAll('.animated-link');
-    const signupLink = document.querySelector('.signup-link'); // "회원가입" 링크 요소 선택
+    const signupLink = document.querySelector('.signup-link.animated-link'); // 수정: 애니메이션 링크만 선택
 
     animatedLinks.forEach(link => {
         let animated = false;
-        let animationClass = 'animate__flipInX'; // 기본 애니메이션 효과
+        let animationClass = 'animate__flipInX';
 
-        // "회원가입" 링크인 경우 애니메이션 효과 변경
         if (link === signupLink) {
             animationClass = 'animate__hinge';
         }
@@ -22,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
             let currentLink = this;
 
             function handleAnimationEnd() {
-                currentLink.classList.remove('animate__animated', animationClass); // animationClass 변수 사용
+                currentLink.classList.remove('animate__animated', animationClass);
                 animated = false;
                 currentLink.removeEventListener('animationend', handleAnimationEnd);
             }
